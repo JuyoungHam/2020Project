dd9a9efe38efc60dcdf95161b66999290a7fddda
Jinny-ham <hamjuyoung11@gmail.com>
2020-07-08 17:33:13 +0900

    kakao 로그인 진행중 kakao 사용자들은 별도로 데이터베이스에 넣을 것

--- MyProject73029/app/src/main/java/com/ici/myproject73029/Constant.java
+++ MyProject73029/app/src/main/java/com/ici/myproject73029/Constant.java
@@ -1,5 +1,16 @@
 package com.ici.myproject73029;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import static com.kakao.util.helper.Utility.getPackageInfo;
+
 public class Constant {
     public static final String TAG = "FirebaseLog";
 
@@ -13,4 +24,23 @@ public class Constant {
     public static final String TWITTER_AUTH = "twitter";
     public static final String FACEBOOK_AUTH = "facebook";
     public static final String KAKAO_AUTH = "kakao";
+
+
+    //How to get hashcode
+    public static String getKeyHash(final Context context) {
+        PackageInfo packageInfo = getPackageInfo(context, PackageManager.GET_SIGNATURES);
+        if (packageInfo == null)
+            return null;
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                return android.util.Base64.encodeToString(md.digest(), android.util.Base64.NO_WRAP);
+            } catch (NoSuchAlgorithmException e) {
+                Log.w("MainActivity", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
+        return null;
+    }
 }
