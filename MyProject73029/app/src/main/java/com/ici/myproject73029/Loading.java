a68d6352c0468233fb7e6fe9c0c42cead0140552
choisubin2 <67613481+choisubin2@users.noreply.github.com>
2020-08-13 10:14:13 +0900

    로딩화면 재수정

--- MyProject73029/app/src/main/java/com/ici/myproject73029/Loading.java
+++ MyProject73029/app/src/main/java/com/ici/myproject73029/Loading.java
@@ -2,6 +2,7 @@ package com.ici.myproject73029;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,8 +12,18 @@ public class Loading extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent=new Intent(getApplicationContext(),MainActivity.class);
-        startActivity(intent);
-        Loading.this.finish();
+        setContentView(R.layout.activity_loading);
+        Handler handler = new Handler();
+        handler.postDelayed(new startLoading(), 1000);
+        getSupportActionBar().hide();
+    }
+
+    public class startLoading implements Runnable {
+        @Override
+        public void run() {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            Loading.this.finish();
+        }
     }
 }
